7021f1ff5736a66979765d475ee59c765707a452
martin <martdevelopers254@gmail.com>
2020-02-26 08:06:21 +0300

    Implemented Networking Commits Dashboard

---
+++ resources/views/Core/networking/assets/_partials/navbar.php
@@ -0,0 +1,180 @@
+
+            <div class="navbar-custom">
+                <ul class="list-unstyled topnav-menu float-right mb-0">
+
+                    <li class="d-none d-sm-block">
+                        <form action="devlan_pages_advance_search.php" class="app-search">
+                            <div class="app-search-box">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search...">
+                                    <div class="input-group-append">
+                                        <button class="btn" type="submit">
+                                            <i class="fe-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </li>
+        
+                    <li class="dropdown notification-list">
+                        <a class="nav-link dropdown-toggle  waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            <i class="fe-bell noti-icon"></i>
+
+                            <?php
+                                //count Number of Notifications for logged in user.
+                                $user_id = $_SESSION['user_id'];
+                                $result ="SELECT count(*)  FROM  notifications WHERE  user_id = ? ";
+                                $stmt = $mysqli->prepare($result);
+                                $stmt->bind_param('i', $user_id);
+                                $stmt->execute();
+                                $stmt->bind_result($notifications);
+                                $stmt->fetch();
+                                $stmt->close();
+                            ?>
+
+                            <span class="badge badge-danger rounded-circle noti-icon-badge"><?php echo $notifications;?></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-lg">
+
+                            <!-- item-->
+                            
+                            <div class="slimscroll noti-scroll">
+                            <?php
+                                //get logged in user notifications
+                                $user_id = $_SESSION['user_id'];
+                                $ret="SELECT  * FROM  notifications WHERE  user_id = ? LIMIT 15";
+                                $stmt= $mysqli->prepare($ret) ;
+                                $stmt->bind_param('i',$user_id);
+                                $stmt->execute() ;//ok
+                                $res=$stmt->get_result();
+
+                                while($row=$res->fetch_object())
+                                {
+                                //trim timestamps to DD-YY-MMMM
+                                    $DT = $row->n_tstamp;
+                            ?>
+                                <!-- item-->
+                                <a href="javascript:void(0);" class="dropdown-item notify-item active">
+                                    <div class="notify-icon">
+                                        <img src="../assets/img/devlan-logo.png" class="img-fluid rounded-circle" alt="" /> </div>
+                                    <p class="notify-details"><?php echo $row->n_name;?></p>
+                                    <p class="text-muted mb-0 user-msg">
+                                        <small><?php echo $row->n_desc;?></small>
+                                    </p>
+                                </a>
+                            <?php }?>    
+
+                            </div>
+
+                            <!-- All-->
+                            <a href="devlan_pages_account.php#notifications" class="dropdown-item text-center text-primary notify-item notify-all">
+                                View all
+                                <i class="fi-arrow-right"></i>
+                            </a>
+
+                        </div>
+                    </li>
+                <?php
+                    $user_id = $_SESSION['user_id'];
+                    $ret="SELECT  * FROM  users WHERE user_id=?";
+                    $stmt= $mysqli->prepare($ret) ;
+                    $stmt->bind_param('i',$user_id);
+                    $stmt->execute() ;//ok
+                    $res=$stmt->get_result();
+
+                    while($row=$res->fetch_object())
+                {
+                ?>
+                    <li class="dropdown notification-list">
+                        <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            <img src="../assets/img/DevLanners/<?php echo $row->dpic;?>" alt="user-image" class="rounded-circle avatar-lg">
+                            <span class="pro-user-name ml-1">
+                                <?php echo $row->username;?> <i class="mdi mdi-chevron-down"></i> 
+                            </span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                            <!-- item-->
+                            <div class="dropdown-header noti-title">
+                                <h6 class="text-overflow m-0">Welcome !</h6>
+                            </div>
+
+                            <!-- item-->
+                            <a href="devlan_pages_account.php" class="dropdown-item notify-item">
+                                <i class="fe-user"></i>
+                                <span>My Account</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="devlan_pages_account_settings.php" class="dropdown-item notify-item">
+                                <i class="fe-settings"></i>
+                                <span>Settings</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="devlan_pages_change_password.php" class="dropdown-item notify-item">
+                                <i class="fe-lock"></i>
+                                <span>Passwords</span>
+                            </a>
+
+                            <div class="dropdown-divider"></div>
+
+                            <!-- item-->
+                            <a href="../devlan_pages_logout_partial.php" class="dropdown-item notify-item">
+                                <i class="fe-log-out"></i>
+                                <span>Logout</span>
+                            </a>
+
+                        </div>
+                    </li>
+                    <?php }?>
+
+
+                </ul>
+
+                <!-- LOGO -->
+                <div class="logo-box">
+                    <a href="" class="logo text-center">
+                        <span class="logo-lg">
+                            <img src="../assets/img/logo-white-xx.png" alt="" height="50">
+                        </span>
+                        <span class="logo-sm">
+                            <img src="../assets/img/logo.png" alt="" height="15">
+                        </span>
+                    </a>
+                </div>
+
+                <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
+                    <li>
+                        <button class="button-menu-mobile waves-effect waves-light">
+                            <i class="fe-menu"></i>
+                        </button>
+                    </li>
+        
+                    <li class="dropdown d-none d-lg-block">
+                        <a class="nav-link dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            Create 
+                            <i class="mdi mdi-chevron-down"></i> 
+                        </a>
+                        <div class="dropdown-menu">
+                            <!-- item-->
+                            <a href="devlan_pages_new_commit.php" class="dropdown-item">
+                                <i class="fe-briefcase mr-1"></i>
+                                <span>New Commit</span>
+                            </a>
+
+                            
+                            <div class="dropdown-divider"></div>
+
+                            <!-- item-->
+                            <a href="https://devlan.martdev.info/f/" target="_blank" class="dropdown-item">
+                                <i class="fe-headphones mr-1"></i>
+                                <span>Devlan Forum</span>
+                            </a>
+
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+
---
+++ resources/views/Core/networking/devlan_pages_account.php
@@ -0,0 +1,287 @@
+<?php
+    session_start();
+    include('assets/configs/config.php');
+    include('assets/configs/checklogin.php');
+    check_login();
+    $user_id = $_SESSION['user_id'];
+    //clear notifications
+        if(isset($_GET['clear_notification']))
+            {
+                $id=intval($_GET['clear_notification']);
+                $adn="DELETE FROM  notifications  WHERE n_id = ?";
+                $stmt= $mysqli->prepare($adn);
+                $stmt->bind_param('i',$id);
+                $stmt->execute();
+                $stmt->close();	 
+             /*
+                Dont Indicate that notification has been cleared
+                    if($stmt)
+                        {
+                            $success = "House Booking Records Deleted";
+                        }
+                    else
+                        {
+                            $err = "Try Again Later";
+                        }
+                */
+            }
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <?php include("assets/_partials/head.php");?>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Topbar Start -->
+            <?php include("assets/_partials/navbar.php");?>
+            <!-- end Topbar -->
+
+            <!-- ========== Left Sidebar Start ========== -->
+                <?php include("assets/_partials/sidebar.php");?>
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start Page Content here -->
+            <!-- ============================================================== -->
+            <?php
+                //Use logged in user session['user_id']
+                $user_id = $_SESSION['user_id'];
+                $ret="SELECT  * FROM  users WHERE user_id=?";
+                $stmt= $mysqli->prepare($ret) ;
+                $stmt->bind_param('i',$user_id);
+                $stmt->execute() ;//ok
+                $res=$stmt->get_result();
+
+                while($row=$res->fetch_object())
+                {
+            ?>
+                <div class="content-page">
+                    <div class="content">
+
+                        <!-- Start Content-->
+                        <div class="container-fluid">
+
+                            <!-- start page title -->
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="page-title-box">
+                                        <div class="page-title-right">
+                                            <ol class="breadcrumb m-0">
+                                                <li class="breadcrumb-item"><a href="javascript: void(0);">DevLan</a></li>
+                                                <li class="breadcrumb-item"><a href="devlan_pages_dashboard.php">Dashboard</a></li>
+                                                <li class="breadcrumb-item active">Profile</li>
+                                            </ol>
+                                        </div>
+                                        <h4 class="page-title"><?php echo $row->username?>'s Profile</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end page title -->
+
+                            <div class="row">
+                                <div class="col-lg-4 col-xl-4">
+                                    <div class="card-box text-center">
+                                        <img src="../assets/img/DevLanners/<?php echo $row->dpic;?>" class="img-thumbnail"  alt="profile-image">
+
+                                        <h4 class="mb-0"><?php echo $row->username;?></h4>
+                                        <hr>
+                                        <div class="text-left mt-3">
+                                            <h4 class="font-13 text-uppercase">Intro | Bio | About:</h4>
+                                            <hr>
+                                            <p class="text-muted font-13 mb-3">
+                                                <?php echo $row->bio;?>
+                                            </p>
+                                            <hr>
+                                            <p class="text-muted mb-2 font-13"><strong>Full Name :</strong> <span class="ml-2"><?php echo $row->fname;?>
+                                                    <?php echo $row->lname;?></span></p>
+
+                                            <p class="text-muted mb-2 font-13"><strong>Mobile :</strong><span class="ml-2"><?php echo $row->phone;?>
+                                            </span></p>
+
+                                            <p class="text-muted mb-2 font-13"><strong>Email :</strong> <span class="ml-2 "><?php echo $row->email;?></span></p>
+
+                                            <p class="text-muted mb-2 font-13"><strong>Date Of Birth :</strong> <span class="ml-2 "><?php echo $row->dob;?></span></p>
+
+                                            <p class="text-muted mb-1 font-13"><strong>Residency :</strong> <span class="ml-2"><?php echo $row->location;?></span></p>
+
+                                            <p class="text-muted mb-1 font-13"><strong>WebSite :</strong> <span class="ml-2">
+                                                <a target = "_blank" href="<?php echo $row->website;?>">
+                                                    <?php echo $row->website;?></span>
+                                                </a>
+                                            </p>
+                                                
+
+                                        </div>
+                                        <hr>
+                                        <ul class="social-list list-inline mt-3 mb-0">
+                                            <li class="list-inline-item">
+                                                <a href="https://linkedin.com/<?php echo $row->in_username;?>" target="_blank" class="social-list-item border-primary text-primary"><i
+                                                        class="mdi mdi-linkedin"></i></a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="https://wa.me/<?php echo $row->phone;?>" target="_blank" class="social-list-item border-success text-success"><i
+                                                        class="mdi mdi-whatsapp"></i></a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="https://twitter.com/<?php echo $row->twt_username;?>" target="blank" class="social-list-item border-info text-info"><i
+                                                        class="mdi mdi-twitter"></i></a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="https://github.com/<?php echo $row->git_username;?>" target="_blank" class="social-list-item border-secondary text-secondary"><i
+                                                        class="mdi mdi-github-circle"></i></a>
+                                            </li>
+                                        </ul>
+                                        <hr>
+                                    </div> <!-- end card-box -->
+
+                                    
+                                </div> <!-- end col-->
+
+                                <div class="col-lg-8 col-xl-8">
+                                    <div class="card-box">
+                                        <ul class="nav nav-pills navtab-bg nav-justified">
+                                            <li class="nav-item">
+                                                <a href="#contribution" data-toggle="tab" aria-expanded="false" class="nav-link active">
+                                                    My Latest Contribution
+                                                </a>
+                                            </li>
+                                            
+                                            <li class="nav-item">
+                                                <a href="#notifications" data-toggle="tab" aria-expanded="false" class="nav-link">
+                                                    Notifications
+                                                </a>
+                                            </li>
+                                        </ul>
+                                        <div class="tab-content">
+                                            <div class="tab-pane active" id="contribution">
+
+                                                <h5 class="mb-4"><i class="mdi mdi-source-pull mr-1"></i>
+                                                    <?php echo $row->username;?> Commits
+                                                </h5>
+
+                                                <ul class="list-unstyled timeline-sm">
+                                                    <?php
+                                                        //get logged in user commits
+                                                        $user_id = $_SESSION['user_id'];
+                                                        $ret="SELECT  * FROM  projects WHERE  user_id = ?  ORDER BY `projects`.`date_created` DESC LIMIT 30 ";
+                                                        $stmt= $mysqli->prepare($ret) ;
+                                                        $stmt->bind_param('i',$user_id);
+                                                        $stmt->execute() ;//ok
+                                                        $res=$stmt->get_result();
+
+                                                    while($row=$res->fetch_object())
+                                                    {
+                                                        //trim timestamps to DD-YY-MMMM
+                                                            $DT = $row->date_created;
+                                                    ?>
+                                                        <li class="timeline-sm-item">
+                                                            <span class="timeline-sm-date">
+                                                                <?php echo date("d-M-Y", strtotime($DT));?>
+                                                            </span>
+                                                            <h5 class="mt-0 mb-1">
+                                                                <a href="">
+                                                                    <?php echo $row->project_name;?>
+                                                                </a>
+                                                            </h5>
+                                                            <p>
+                                                                <u>
+                                                                    <?php echo $row->project_category;?>
+                                                                </u>
+                                                            </p>
+                                                            <p class="text-muted mt-2">
+                                                                <?php echo $row->project_desc;?>
+                                                            </p>
+                                                            <!--View Commit-->
+                                                            <a href="devlan_pages_view_single_commit.php?project_id=<?php echo $row->project_id;?>" class="float-right btn btn-outline-info"><i class="fa fa-eye"></i> <i class="mdi mdi-source-pull"></i> View Commit</a><br>
+                                                            
+                                                            <hr>
+
+                                                        </li>
+                                                    <?php }?>
+                                                </ul>
+
+                                            </div> <!-- end commit tab-->
+
+                                            <div class="tab-pane " id="notifications">
+
+                                                <h5 class="mb-4 text-uppercase"><i class="mdi mdi-bell mr-1"></i>
+                                                    Notifications </h5>
+
+                                                <ul class="list-unstyled timeline-sm">
+                                                    <?php
+                                                        //get logged in user notifications
+                                                        $user_id = $_SESSION['user_id'];
+                                                        $ret="SELECT  * FROM  notifications WHERE  user_id = ? LIMIT 15";
+                                                        $stmt= $mysqli->prepare($ret) ;
+                                                        $stmt->bind_param('i',$user_id);
+                                                        $stmt->execute() ;//ok
+                                                        $res=$stmt->get_result();
+
+                                                        while($row=$res->fetch_object())
+                                                        {
+                                                        //trim timestamps to DD-YY-MMMM
+                                                            $DT = $row->n_tstamp;
+                                                    ?>
+                                                        <li class="timeline-sm-item">
+                                                            <span class="timeline-sm-date"><?php echo date("d-M-Y", strtotime($DT));?></span>
+                                                            <h5 class="mt-0 mb-1"><?php echo $row->n_name;?></h5>
+                                                            <p><?php echo $row->n_from;?></p>
+                                                            <p class="text-muted mt-2">
+                                                                <?php echo $row->n_desc;?>
+                                                            </p>
+
+                                                            <a href="devlan_pages_account.php?clear_notification=<?php echo $row->n_id;?>" class="float-right btn btn-outline-danger"><i class="fa fa-trash"></i> <i class="fa fa-bell"></i> Clear</a><br>
+
+                                                        </li><hr>
+
+                                                    <?php }?>
+                                                </ul>
+
+                                            </div>
+                                            <!-- end notifications content-->
+
+
+                                        </div> <!-- end tab-content -->
+                                    </div> <!-- end card-box-->
+
+                                </div> <!-- end col -->
+                            </div>
+                            <!-- end row-->
+
+                        </div> <!-- container -->
+
+                    </div> <!-- content -->
+
+                    <!-- Footer Start -->
+                <?php include("assets/_partials/footer.php");?>
+                    <!-- end Footer -->
+
+                </div>
+            <?php }?>
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+       
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.min.js"></script>
+
+    </body>
+
+
+</html>
